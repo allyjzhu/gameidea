f0fe622f854e488ec0e84c4e55923c2fbf4c0e81
allyjzhu <70976920+allyjzhu@users.noreply.github.com>
2020-10-06 13:30:24 -0700

    Add files via upload

---
+++ gameiea/index.js
@@ -0,0 +1,35 @@
+class Clarabell {
+
+    constructor(friends){
+      const {AJ, Nico, Talia} = friends;
+      this.name = 'Clarabell';
+      this.home = 'Tokyo';
+      this.friends = [AJ, Nico, Talia];
+      this.goal = catchfrog; 
+  
+    }
+  
+  
+    hearfrog(){
+      //use keyboard input to move characters, 
+      //if move character closer to frog, frog vol increase
+  
+    }
+  
+    catchfrog(){
+        //click input from computer;
+    }
+  
+    createhandshake(){
+          //memorize order of handshakes w friends;
+    }
+  
+    taketrain(){
+        //memorize train schedules or how to get places
+    }
+  
+  
+  
+  
+  }
+  
